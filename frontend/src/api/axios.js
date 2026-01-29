016ddc43eae84d94da2aa53b244fd8b723b43d2e
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-01-30 01:53:25 +0530

    updated UI

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api"|| import.meta.env.VITE_API_URL,
+  baseURL: "https://task-management-backend-green-delta.vercel.app/api",
  
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;         
   }
   return config;
 });
